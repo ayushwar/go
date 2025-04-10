56f3e91d3058463f672d2a75b1fb1edbdc296a68
ayushwar <ayush.ahirwar2226@gmail.com>
2025-04-10 20:39:15 +0530

    basic web server

---
+++ 3_web_server/go.mod
@@ -0,0 +1,5 @@
+module 3_web_server
+
+go 1.24.2
+
+require github.com/joho/godotenv v1.5.1
---
+++ 3_web_server/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/joho/godotenv"
+
+)
+func formpagehandeler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		http.ServeFile(w, r, "statics/form.html")
+		return
+	}
+
+	if r.Method=="POST"{
+		err:=r.ParseForm()
+		if err!=nil{
+			log.Fatal(err)
+
+		}
+
+		name:=r.FormValue("name")
+		age:=r.FormValue("age")
+
+		fmt.Fprintln(w,"form submitted succesfully")
+
+		fmt.Fprintln(w,"name=",name)
+		fmt.Fprintln(w,"age",age)
+
+
+	}
+
+}
+
+
+
+
+
+
+func indexhandeler(w http.ResponseWriter , r* http.Request){
+if r.URL.Path!="/index"{
+	http.Error(w,"404 not found",http.StatusNotFound)
+	return
+
+}
+if r.Method!="GET"{
+	http.Error(w,"not a valid",http.StatusNotFound)
+	return
+}
+http.ServeFile(w,r,"statics/index.html")
+}
+
+
+
+func main(){
+	err:=godotenv.Load()
+	if err != nil {
+        log.Fatal("Error loading .env file")
+
+    }
+
+
+	port:=os.Getenv("PORT")
+	if port==""{
+		port="8080"
+	}
+
+	fileserver:=http.FileServer(http.Dir("statics"))
+	http.Handle("/",fileserver)
+	http.HandleFunc("/index",indexhandeler)
+	http.HandleFunc("/form",formpagehandeler)
+	fmt.Println("serever is starting on port 8000")
+	err =http.ListenAndServe(":"+port,nil)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+
+}
